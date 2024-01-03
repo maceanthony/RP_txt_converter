01aa9af55e5eb9790ebb664d5af0119625f05285
windu-ant <maceanthony@gmail.com>
2024-01-03 14:11:15 -0500

    updated for new colors

--- RP_Color_Txt_Convert.py
+++ RP_Color_Txt_Convert.py
@@ -36,30 +36,56 @@ def convert_color_codes():
 
     # Define the color code mappings
     color_codes = {
-        '&x': '[/color][color=#000000]', #black
-        '&z': '[/color][color=#808080]', #dgrey
-        '&w': '[/color][color=#C0C0C0]', #grey
-        '&W': '[/color][color=#FFFFFF]', #white
-        '&P': '[/color][color=#FF00FF]', #pink
-        '&V': '[/color][color=#AF5FAF]', #violet blue
-        '&R': '[/color][color=#FF0000]', #red
-        '&v': '[/color][color=#AF005F]', #dark magenta
-        '&p': '[/color][color=#800080]', #dark pink
-        '&r': '[/color][color=#800000]', #red
-        '&o': '[/color][color=#AF5F00]', #brown
-        '&O': '[/color][color=#FFAF00]', #orange
-        '&y': '[/color][color=#AFAF00]', #gold
-        '&Y': '[/color][color=#FFFF00]', #yellow
-        '&g': '[/color][color=#008000]', #dgreen
-        '&n': '[/color][color=#5FAF00]', #chartreuse
-        '&G': '[/color][color=#00FF00]', #green
-        '&N': '[/color][color=#00FFAF]', #aqua
-        '&C': '[/color][color=#00FFFF]', #cyan
-        '&c': '[/color][color=#008080]', #dcyan
-        '&h': '[/color][color=#008787]', #cadet blue
-        '&H': '[/color][color=#5F87FF]', #cornflower blue
-        '&b': '[/color][color=#0000FF]', #dark blue
-        '&B': '[/color][color=#0000AF]', #even darker blue
+
+        '&x': '[/color][color=#000000]',
+        '&z': '[/color][color=#808080]',
+        '&w': '[/color][color=#C0C0C0]',
+        '&W': '[/color][color=#FFFFFF]',
+        '&P': '[/color][color=#FF00FF]',
+        '&p': '[/color][color=#800080]',
+        '&R': '[/color][color=#FF0000]',
+        '&r': '[/color][color=#800000]',
+        '&O': '[/color][color=#808000]',
+        '&Y': '[/color][color=#FFFF00]',
+        '&g': '[/color][color=#008000]',
+        '&G': '[/color][color=#00FF00]',
+        '&C': '[/color][color=#00FFFF]',
+        '&c': '[/color][color=#008787]',
+        '&B': '[/color][color=#0000FF]',
+        '&b': '[/color][color=#0000AF]',
+        '}a': '[/color][color=#005FAF]',
+        '}A': '[/color][color=#0087FF]',
+        '}b': '[/color][color=#5F87FF]',
+        '}B': '[/color][color=#5FD7FF]',
+        '}c': '[/color][color=#00AFAF]',
+        '}C': '[/color][color=#00D7FF]',
+        '}e': '[/color][color=#4E4E4E]',
+        '}E': '[/color][color=#5F5F5F]',
+        '}g': '[/color][color=#00AF00]',
+        '}G': '[/color][color=#87FF00]',
+        '}j': '[/color][color=#00AF5F]',
+        '}J': '[/color][color=#00FF87]',
+        '}l': '[/color][color=#5FAF00]',
+        '}L': '[/color][color=#87FF00]',
+        '}m': '[/color][color=#AF00AF]',
+        '}M': '[/color][color=#D700AF]',
+        '}o': '[/color][color=#AF5F00]',
+        '}G': '[/color][color=#FF8700]',
+        '}p': '[/color][color=#AF005F]',
+        '}P': '[/color][color=#FF0087]',
+        '}r': '[/color][color=#AF0000]',
+        '}R': '[/color][color=#D70000]',
+        '}s': '[/color][color=#878787]',
+        '}S': '[/color][color=#D7D7D7]',
+        '}t': '[/color][color=#875F00]',
+        '}T': '[/color][color=#D7AF00]',
+        '}v': '[/color][color=#5F00AF]',
+        '}V': '[/color][color=#8700FF]',
+        '}w': '[/color][color=#AFAFAF]',
+        '}W': '[/color][color=#DADADA]',
+        '}y': '[/color][color=#AFAF00]',
+        '}Y': '[/color][color=#FFD700]',
+
     }
 
     # Replace the color code sequences
@@ -72,7 +98,7 @@ def convert_color_codes():
 
     updated_content = content.replace("[/color]", "", 1)
 
-    with open(filename, 'w') as file:
+    with open(filename, 'w', encoding='utf-8-sig') as file:
         file.write(updated_content)
 
     with open(filename, 'a') as file:
